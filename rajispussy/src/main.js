20c0cf13096db739501d904f8d188db4655b4aaf
steenn30 <steenn30@gmail.com>
2025-12-25 09:58:43 -0500

    Fix hash navigation and remove debug logs

---
+++ rajispussy/src/main.js
@@ -0,0 +1,33 @@
+import React, { Component } from './runtime/react.js';
+import { createRoot } from './runtime/react-dom.js';
+import { HashRouter, Route } from './router.js';
+import HomePage from './components/HomePage.js';
+import ScreenwriterProfileCustomerView from './components/ScreenwriterProfileCustomerView.js';
+import { profiles } from './data/seed.js';
+
+class App extends Component {
+  render() {
+    return React.createElement(
+      HashRouter,
+      null,
+      React.createElement(Route, { path: '/', element: React.createElement(HomePage, null) }),
+      React.createElement(Route, {
+        path: '/author/:name',
+        component: ProfileRoute,
+      }),
+    );
+  }
+}
+
+class ProfileRoute extends Component {
+  render() {
+    const path = window.location.hash.replace(/^#/, '').replace(/^\//, '');
+    const [, encodedName] = path.split('/') || [];
+    const name = decodeURIComponent(encodedName || '');
+    const profile = profiles[name];
+    return React.createElement(ScreenwriterProfileCustomerView, { profile });
+  }
+}
+
+const root = createRoot(document.getElementById('root'));
+root.render(React.createElement(App, null));
